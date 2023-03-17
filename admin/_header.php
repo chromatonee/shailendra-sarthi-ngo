da2488b56a08198caefc481bb77880165587f6ff
suprotivesarkar <suprotivesarkar@gmail.com>
2023-03-18 01:53:58 +0530

    second commit

--- admin/_header.php
+++ admin/_header.php
@@ -1,4 +1,7 @@
 <?php debug_backtrace() || header("Location: 404"); ?>
+<!-- <base href="http://shailendra-sarthi.000webhostapp.com/"> -->
+<meta name="theme-color" content="#d3cd8f">
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
